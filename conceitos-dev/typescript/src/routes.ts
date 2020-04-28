4d2927c7bcf53d46d95143240a9d14a9ae6463c7
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2020-04-28 19:22:02 -0300

    Modulo sobre TypeScript

---
+++ conceitos-dev/typescript/src/routes.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
+
+export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'nurycaroline@gmail.com',
+    password: '123456',
+    techs: [
+      'NodeJS',
+      'React',
+      { title: 'Javascript', experience: 100 }
+    ]
+  });
+
+  return response.json({ message: 'Hello World' });
+}
---
+++ conceitos-dev/typescript/src/services/CreateUser.ts
@@ -0,0 +1,18 @@
+interface TechObject {
+  title: string,
+  experience: number
+}
+
+interface CreateUserData {
+  name?: string,
+  email: string,
+  password: string,
+  techs: Array<string | TechObject> // if only strings: string[]
+}
+
+export default function createUser({ name, email, password }: CreateUserData) {
+  const user = {
+    name, email, password
+  }
+  return user
+}
