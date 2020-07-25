07511037e3956c0bae23ab0b10a549b6384940ae
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2020-07-25 16:23:41 -0300

    Primeiro teste

--- gobarber-back/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ gobarber-back/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,19 @@
-test("sum tow numbers", () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from "@modules/appointments/repositories/fakes/FakeAppointmentsRepository";
+import CreateAppointmentService from "./CreateAppointmentService";
+
+describe("CreateAppointment", () => {
+  it("should be able to create a new appointment", async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: "1234",
+    });
+
+    expect(appointment).toHaveProperty("id");
+    expect(appointment.provider_id).toBe("1234");
+  });
 });
