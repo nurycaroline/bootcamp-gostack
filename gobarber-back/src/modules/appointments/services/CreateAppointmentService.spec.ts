93f35b58d4997cdafa7c7c2b7b116b2c6ef80b4f
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2020-07-25 16:40:10 -0300

    Coverage report; Teste de agendamento

--- gobarber-back/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ gobarber-back/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,4 +1,5 @@
 import FakeAppointmentsRepository from "@modules/appointments/repositories/fakes/FakeAppointmentsRepository";
+import AppError from "@shared/errors/AppError";
 import CreateAppointmentService from "./CreateAppointmentService";
 
 describe("CreateAppointment", () => {
@@ -15,5 +16,26 @@ describe("CreateAppointment", () => {
 
     expect(appointment).toHaveProperty("id");
     expect(appointment.provider_id).toBe("1234");
+  });
+
+  it("should not be able to create two appointments on the same time", async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: "1234",
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: "1234",
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
