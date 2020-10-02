bb375ebe74043a45c3bdf60306c7d2f6d8c52f76
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2020-10-02 15:06:41 -0300

    Cache de agendamentos

--- gobarber-back/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ gobarber-back/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -27,18 +27,24 @@ class ListProviderAppointmentsService {
     month,
     year,
   }: IRequestDTO): Promise<Appointment[]> {
-    const cacheData = await this.cacheProvider.recover("dhasd");
+    const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
 
-    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
-      {
-        provider_id,
-        day,
-        month,
-        year,
-      },
+    let appointments = await this.cacheProvider.recover<Appointment[]>(
+      cacheKey,
     );
 
-    // await this.cacheProvider.save("ada", "dhasd");
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+        {
+          provider_id,
+          day,
+          month,
+          year,
+        },
+      );
+
+      await this.cacheProvider.save(cacheKey, appointments);
+    }
 
     return appointments;
   }
